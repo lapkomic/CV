1cb41f12f3a647b491003bd592c2f742237a88b3
lapkomic <134934759+lapkomic@users.noreply.github.com>
2023-05-30 00:13:52 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+
+let sections = document.querySelectorAll('section');
+let = navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll=()=>
+{
+	let header = document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 100);
+	
+	sections.forEach(sec => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop-100;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute('id');
+		
+		if(top >= offset && top < offset + height)
+		{
+			navLinks.forEach(link =>{
+				link.classList.remove('active');				
+			});
+			
+			document.querySelector('header nav a[href*='+ id +']').classList.add('active');			
+			sec.classList.add('show-animate');
+		}
+		else
+		{
+			sec.classList.remove('show-animate');
+		}
+	});	
+	
+	menuIcon.classList.remove('bx-x');
+	navbar.classList.remove('active');
+}
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle('bx-x');
+	navbar.classList.toggle('active');
+}
+
+let submitBtn = document.querySelector('#submit');
+
+
+	  
